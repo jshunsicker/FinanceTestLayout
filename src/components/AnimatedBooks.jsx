67e160e15703fdcf9cd26d292bb178558ae98115
jeremiah <sc.arletknight1807@gmail.com>
2020-01-28 19:57:30 -0500

    Moved main content to own component

--- src/components/AnimatedBooks.jsx
+++ src/components/AnimatedBooks.jsx
@@ -46,96 +46,63 @@ export default function AnimatedBooks() {
         </div>
       </section>
       <section className="bp__content">
-        <h1 className="bp__title">Most Popular Picks</h1>
-        <div className="bp__book-list">
-          {bookResponseData.items.map(bookResponse => {
-            const book = bookResponse.volumeInfo;
-            return (
-              <div className="bp__book-card">
-                <img
-                  src={book.imageLinks.smallThumbnail}
-                  alt={`${book.title}`}
-                  className="bp__book-img"
-                />
-                <div className="bp__book-info">
-                  <h2 className="bp__book-title">{book.title}</h2>
-                  <div>
-                    <div className="bp__ratings">
-                      {Array(5)
-                        .fill("")
-                        .map((x, index) => (
-                          <svg
-                            className={`bp__icon bp__icon--star ${
-                              index < book.averageRating
-                                ? "bp__icon--yellow"
-                                : "bp__icon--grey"
-                            }`}
-                          >
-                            <use href="./img/symbol-defs.svg#icon-star-full" />
-                          </svg>
-                        ))}
-                    </div>
-                    <a
-                      className="bp__link"
-                      href={book.previewLink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      See reviews ({book.ratingsCount})
-                    </a>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        <BookListMainContent bookResponseData={bookResponseData} />
       </section>
+
       <section className="bp__audible">
         <audio></audio>
       </section>
     </div>
-    // <BooksContainer className="book-search__results">
-    //   {bookResponseData.map(googleBook => {
-    //     return (
-    //       <BookPanel className="book-panel" key={googleBook.id}>
-    //         <h3 className="book-panel__title">{googleBook.volumeInfo.title}</h3>
-    //         <h4 className="book-panel__author">
-    //           <em>{googleBook.volumeInfo.authors[0]}</em>
-    //         </h4>
-    //         <div className="book-panel__image-wrapper">
-    //           {googleBook.volumeInfo.imageLinks && (
-    //             <Image
-    //               className="book-panel__image"
-    //               alt={googleBook.volumeInfo.title}
-    //               src={
-    //                 googleBook.volumeInfo.imageLinks.thumbnail ||
-    //                 googleBook.volumeInfo.imageLinks.smallThumbnail
-    //               }
-    //             />
-    //           )}
-    //         </div>
-    //         <div className="book-panel__description">
-    //           <p className="book-panel__description-text">
-    //             {googleBook.volumeInfo.description}
-    //           </p>
-    //         </div>
-    //         <div className="book-panel__links">
-    //           <div className="book-panel__rating">
-    //             {googleBook.volumeInfo.averageRating}
-    //           </div>
-    //           <div className="book-panel__preview">
-    //             <a
-    //               href={googleBook.volumeInfo.previewLink}
-    //               target="_blank"
-    //               rel="noopener noreferrer"
-    //             >
-    //               Preview
-    //             </a>
-    //           </div>
-    //         </div>
-    //       </BookPanel>
-    //     );
-    //   })}
-    // </BooksContainer>
   );
 }
+
+export const BookListMainContent = props => {
+  const { bookResponseData } = props;
+  return (
+    <React.Fragment>
+      <h1 className="bp__title">Most Popular Picks</h1>
+      <div className="bp__book-list">
+        {bookResponseData.items.map(bookResponse => {
+          const book = bookResponse.volumeInfo;
+          return (
+            <div className="bp__book-card">
+              <img
+                src={book.imageLinks.smallThumbnail}
+                alt={`${book.title}`}
+                className="bp__book-img"
+              />
+              <div className="bp__book-info">
+                <h2 className="bp__book-title">{book.title}</h2>
+                <div>
+                  <div className="bp__ratings">
+                    {Array(5)
+                      .fill("")
+                      .map((x, index) => (
+                        <svg
+                          className={`bp__icon bp__icon--star ${
+                            index < book.averageRating
+                              ? "bp__icon--yellow"
+                              : "bp__icon--grey"
+                          }`}
+                        >
+                          <use href="./img/symbol-defs.svg#icon-star-full" />
+                        </svg>
+                      ))}
+                  </div>
+                  <a
+                    className="bp__link"
+                    href={book.previewLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    See reviews ({book.ratingsCount})
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
+};
